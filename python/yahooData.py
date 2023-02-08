ee1bcd347a3ea3f9d4ea02f9a60417e62f2c5e9f
CJ0423 <b09785157888@gmail.com>
2023-02-08 09:18:39 +0800

    k

--- python/yahooData.py
+++ python/yahooData.py
@@ -26,9 +26,11 @@ def list_stock(input_type):
 
     datalist = []
     for col in soup.find_all_next('li'):
-        datalist.append(
-            [row.text for row in col.find_all('div', class_='Lh(20px) Fw(600) Fz(16px) Ell')]
-        )
+        data = [row.text for row in col.find_all('div', class_='Lh(20px) Fw(600) Fz(16px) Ell')]
+    if len(data) > 0:
+        datalist.append(data)
+
+
     title = ["股票名稱"]
 
     df = pd.DataFrame(datalist[0:], columns=title)
